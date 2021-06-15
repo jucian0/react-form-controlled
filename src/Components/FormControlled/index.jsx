5bcdf64f83dd3a2a093f96e1c487390240e3f2b3
Juciano <juciano@outlook.com.br>
2021-06-15 10:16:17 -0300

    Update index.jsx

--- src/Components/FormControlled/index.jsx
+++ src/Components/FormControlled/index.jsx
@@ -1,61 +1,65 @@
 import React, { useState, useEffect } from "react";
 import Input from "./Input";
-import {FormValidations} from './index.validations'
-import useValidation from './../../hooks/useValidation'
+import { FormValidations } from "./index.validations";
+import useValidation from "./../../hooks/useValidation";
 
 const initialFormState = {
-   name:'',
-   email:'',
-   password:''
-}
+  name: "",
+  email: "",
+  password: ""
+};
 
 const UserForm = () => {
+  const [form, setForm] = useState(initialFormState);
+  const { errors } = useValidation(form, FormValidations);
 
-   const [form, setForm] = useState(initialFormState)
-   const {errors} = useValidation(form, FormValidations)
-   
-   const setInput = (newValue)=>{
-      setForm(form => ({...form, ...newValue}))
-   }
+  function setInput(inputName) {
+    return (e) => {
+      const newValue = { [inputName]: e.target.value };
+      return setForm((form) => ({ ...form, ...newValue }));
+    };
+  }
 
-   return (
-      <>
-         <h3>Form Controlled</h3>
-         <form>
-               <div className="form-group">
-                  <Input
-                     name="name"
-                     onChange={e => setInput({ name: e.target.value })}
-                     label="Name"
-                     value={form.name}
-                     error={errors.name}
-                  />
-               </div>
-               <div className="form-group">
-                  <Input
-                     name="email"
-                     onChange={e => setInput({ email: e.target.value })}
-                     label="E-mail"
-                     value={form.email}
-                     error={errors.email}
-                  />
-               </div>
-               <div className="form-group">
-                  <Input
-                     name="password"
-                     onChange={e => setInput({ password: e.target.value })}
-                     label="Password"
-                     value={form.password}
-                     error={errors.password}
-                  />
-               </div>
+  return (
+    <>
+      <h3>Form Controlled</h3>
+      <form>
+        <div className="form-group">
+          <Input
+            name="name"
+            onChange={setInput("name")}
+            label="Name"
+            value={form.name}
+            error={errors.name}
+          />
+        </div>
+        <div className="form-group">
+          <Input
+            name="email"
+            onChange={setInput("email")}
+            label="E-mail"
+            value={form.email}
+            error={errors.email}
+          />
+        </div>
+        <div className="form-group">
+          <Input
+            name="password"
+            onChange={setInput("password")}
+            label="Password"
+            value={form.password}
+            error={errors.password}
+          />
+        </div>
 
-               <div className="form-group">
-                  <button type="button" className="btn btn-primary">Submit</button>
-               </div>
-         </form>
-      </>
-   );
-}
+        <div className="form-group">
+          <button type="button" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
 
 export default UserForm;
